907091c04332c4c4d46337bc1f46cb20762d58c9
Patrick Franco <patrickxfranco@gmail.com>
2025-06-12 02:10:40 -0300

    Atualiza o título da página inicial para "Commando"; ajusta a configuração do servidor no Vite para especificar host e porta.

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default {
   plugins: [tailwindcss(), tsconfigPaths()],
+  server: {
+    host: '192.168.57.200',
+    port: 5173,
+  },
 } satisfies UserConfig;
