25aea08df197c300f3ea18af924b2dcc5e07bf0d
Patrick Franco <patrickxfranco@gmail.com>
2025-06-12 13:04:10 -0300

    Ajusta a classe do componente MobileCard para garantir que a imagem ocupe toda a largura disponível.

--- src/pages/home/MobileCard.tsx
+++ src/pages/home/MobileCard.tsx
@@ -39,7 +39,7 @@ export function MobileCard({ imdbID }: MobileCardProps) {
       <img
         src={data?.short.image}
         alt={`Imagem de ${data?.short.name}`}
-        className="brightness-50 hover:brightness-100 active:brightness-100 grayscale-25 hover:grayscale-0 active:grayscale-0 hover:saturate-150 active:saturate-150 object-cover aspect-video hover:scale-110 active:scale-110 transition-all"
+        className="brightness-50 hover:brightness-100 active:brightness-100 grayscale-25 hover:grayscale-0 active:grayscale-0 hover:saturate-150 active:saturate-150 w-full object-cover aspect-video hover:scale-110 active:scale-110 transition-all"
       />
       <span className="top-0 left-0 absolute group-active:opacity-0 group-hover:opacity-0 p-4 font-bold text-[28px] text-white uppercase transition-all pointer-events-none">
         {data?.short.name}
