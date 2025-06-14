9ecc73963f6dc9275546f1a65b22f21a653548af
Patrick Franco <patrickxfranco@gmail.com>
2025-06-14 12:17:38 -0300

    feat: add dropdown menu component and integrate into LogoArea
    - Added @radix-ui/react-dropdown-menu dependency to package.json and package-lock.json.
    - Implemented DropdownMenu in LogoArea component for better navigation.
    - Created DropdownMenuItem component for dropdown items.
    - Added Separator component using @radix-ui/react-separator for visual separation in dropdown.

--- src/components/Header/LogoArea.tsx
+++ src/components/Header/LogoArea.tsx
@@ -1,16 +1,52 @@
-import { Plus } from 'lucide-react';
+import { Plus, Globe, NotebookPen } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import type { ReactElement } from 'react';
+
+type DropDownMenuItemProps = {
+  itemName: string;
+  icon?: ReactElement;
+};
 
 export function LogoArea() {
   return (
     <div id="logo" className="max-w-min">
-      <Button asChild type="button" variant="ghost" size="icon" className="w-12 h-12">
-        <div>
-          <span className="sr-only">Voltar para home</span>
-          <Plus />
-        </div>
-      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button asChild type="button" variant="ghost" size="icon" className="w-12 h-12">
+            <div>
+              <span className="sr-only">Voltar para home</span>
+              <Plus />
+            </div>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="bg-background/90 backdrop-blur-lg ml-4 min-w-max">
+          <DropdownMenuLabel className="text-[12px] text-muted-foreground">adicionar conteúdo</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropDownMenuItem icon={<Globe />} itemName="Da API" />
+            <DropDownMenuItem icon={<NotebookPen />} itemName="Manualmente" />
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
+  );
+}
+
+function DropDownMenuItem({ itemName, icon }: DropDownMenuItemProps) {
+  return (
+    <DropdownMenuItem className="flex flex-row items-center gap-2 p-3">
+      {icon ? icon : null}
+      {itemName}
+    </DropdownMenuItem>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default {
   plugins: [tailwindcss(), tsconfigPaths()],
   server: {
     allowedHosts: true,
-    host: '192.168.178.159',
+    host: '192.168.42.126',
     port: 5173,
   },
 } satisfies UserConfig;
